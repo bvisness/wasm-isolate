95772434a5bc004c9f0ddd5c6ee6c5d4017f5c0b
Ben Visness <bvisness@mozilla.com>
2025-01-08 15:26:20 -0600

    Parse import section or something
    Mostly untested, huzzah

--- isolate/isolate.go
+++ isolate/isolate.go
@@ -1,11 +1,13 @@
 package isolate
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
 
 	"github.com/bvisness/wasm-isolate/leb128"
+	"github.com/bvisness/wasm-isolate/utils"
 )
 
 func Isolate(wasm io.Reader, out io.Writer, funcs []int) error {
@@ -18,6 +20,8 @@ func Isolate(wasm io.Reader, out io.Writer, funcs []int) error {
 		return err
 	}
 
+	numFuncs := 0
+
 	for {
 		sectionId, err := p.ReadByte("section id")
 		if errors.Is(err, io.EOF) {
@@ -25,20 +29,68 @@ func Isolate(wasm io.Reader, out io.Writer, funcs []int) error {
 		} else if err != nil {
 			return err
 		}
-		sectionSize, err := p.ReadU32("section size")
+		sectionSize, _, err := p.ReadU32("section size")
 		if err != nil {
 			return err
 		}
 
 		switch sectionId {
-		case 3: // function section
-			numFuncs, err := p.ReadU32("num funcs")
+		case 2: // import section
+			numImports, _, err := p.ReadU32("num imports")
 			if err != nil {
 				return err
 			}
-			for i := range numFuncs {
-				// TODO: I dunno
+			for range numImports {
+				_, err = p.ReadName("import module")
+				if err != nil {
+					return err
+				}
+				_, err = p.ReadName("import name")
+				if err != nil {
+					return err
+				}
+
+				importType, err := p.ReadByte("import type")
+				if err != nil {
+					return err
+				}
+				switch importType {
+				case 0x00: // function
+					numFuncs++
+					_, _, err := p.ReadU32("type of imported function")
+					if err != nil {
+						return err
+					}
+				case 0x01: // table
+					_, err := p.ReadTableType("type of imported table")
+					if err != nil {
+						return err
+					}
+				case 0x02: // memory
+					_, err := p.ReadMemType("type of imported memory")
+					if err != nil {
+						return err
+					}
+				case 0x03: // global
+					_, err := p.ReadGlobalType("type of imported global")
+					if err != nil {
+						return err
+					}
+				case 0x04: // tag
+					_, err := p.ReadTagType("type of imported tag")
+					if err != nil {
+						return err
+					}
+				}
 			}
+		// case 3: // function section
+		// 	numFuncs, _, err := p.ReadU32("num funcs")
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// 	for range numFuncs {
+		// 		// TODO: copy functions that we want to preserve, and track their relocations
+		// 	}
 		// case 9: // element section
 		// TODO: Parse segments of funcref to make sure we don't strip declared funcs
 		// case 10: // code section
@@ -50,19 +102,19 @@ func Isolate(wasm io.Reader, out io.Writer, funcs []int) error {
 		}
 	}
 
-	out.Write([]byte("wow!\n"))
+	fmt.Fprintf(out, "wow! %d imported functions\n", numFuncs)
 
 	return nil
 }
 
 type parser struct {
-	r   io.Reader
+	r   *bufio.Reader
 	cur int
 }
 
 func newParser(r io.Reader) parser {
 	return parser{
-		r:   r,
+		r:   bufio.NewReader(r),
 		cur: 0,
 	}
 }
@@ -78,6 +130,15 @@ func (p *parser) ReadN(thing string, n int) ([]byte, error) {
 	return bytes, nil
 }
 
+func (p *parser) PeekByte(thing string) (byte, error) {
+	at := p.cur
+	bytes, err := p.r.Peek(1)
+	if err != nil {
+		return 0, fmt.Errorf("%s at offset %d: %w", thing, at, err)
+	}
+	return bytes[0], nil
+}
+
 func (p *parser) ReadByte(thing string) (byte, error) {
 	at := p.cur
 	var b [1]byte
@@ -89,14 +150,200 @@ func (p *parser) ReadByte(thing string) (byte, error) {
 	return b[0], nil
 }
 
-func (p *parser) ReadU32(thing string) (uint32, error) {
+func (p *parser) ReadU32(thing string) (uint32, int, error) {
+	v, n, err := p.ReadU64(thing)
+	return uint32(v), n, err
+}
+
+func (p *parser) ReadU64(thing string) (uint64, int, error) {
 	at := p.cur
 	v, n, err := leb128.DecodeU64(p.r)
 	if err != nil {
-		return 0, fmt.Errorf("%s at offset %d: %w", thing, at, err)
+		return 0, n, fmt.Errorf("%s at offset %d: %w", thing, at, err)
 	}
 	p.cur += n
-	return uint32(v), nil
+	return v, n, nil
+}
+
+func (p *parser) ReadS32(thing string) (int32, int, error) {
+	v, n, err := p.ReadS64(thing)
+	return int32(v), n, err
+}
+
+func (p *parser) ReadS64(thing string) (int64, int, error) {
+	at := p.cur
+	v, n, err := leb128.DecodeS64(p.r)
+	if err != nil {
+		return 0, n, fmt.Errorf("%s at offset %d: %w", thing, at, err)
+	}
+	p.cur += n
+	return v, n, nil
+}
+
+func (p *parser) ReadName(thing string) (string, error) {
+	n, _, err := p.ReadU32(thing)
+	if err != nil {
+		return "", err
+	}
+	name, err := p.ReadN(thing, int(n))
+	if err != nil {
+		return "", err
+	}
+	return string(name), nil
+}
+
+func (p *parser) ReadTableType(thing string) (tableType, error) {
+	et, err := p.ReadRefType(fmt.Sprintf("element type for %s", thing))
+	if err != nil {
+		return tableType{}, err
+	}
+	lim, err := p.ReadLimits(fmt.Sprintf("limits for %s", thing))
+	if err != nil {
+		return tableType{}, err
+	}
+	return tableType{
+		et:  et,
+		lim: lim,
+	}, nil
+}
+
+func (p *parser) ReadMemType(thing string) (memType, error) {
+	lim, err := p.ReadLimits(fmt.Sprintf("limits for %s", thing))
+	if err != nil {
+		return memType{}, err
+	}
+	return memType{lim}, nil
+}
+
+func (p *parser) ReadGlobalType(thing string) (globalType, error) {
+	t, err := p.ReadValType(thing)
+	if err != nil {
+		return globalType{}, err
+	}
+	mut, err := p.ReadByte(thing)
+	if err != nil {
+		return globalType{}, err
+	}
+
+	return globalType{
+		mut: mut == 0x01,
+		t:   t,
+	}, nil
+}
+
+func (p *parser) ReadTagType(thing string) (uint32, error) {
+	_, err := p.ReadByte(thing)
+	if err != nil {
+		return 0, err
+	}
+	idx, _, err := p.ReadU32(thing)
+	return idx, err
+}
+
+func (p *parser) ReadValType(thing string) (valType, error) {
+	at := p.cur
+
+	t, err := p.ReadByte(thing)
+	if err != nil {
+		return valType{}, err
+	}
+
+	switch tc := typeCode(t); tc {
+	case __rtNonNull, __rtNull:
+		ht, err := p.ReadHeapType(thing)
+		if err != nil {
+			return valType{}, err
+		}
+		return valType{
+			isRef: true,
+			refType: refType{
+				null: tc == __rtNull,
+				ht:   ht,
+			},
+		}, nil
+	default:
+		if tc.IsNumType() || tc.IsVecType() {
+			return valType{
+				numOrVecType: tc,
+			}, nil
+		} else if tc.IsHeapType() {
+			return valType{
+				isRef: true,
+				refType: refType{
+					null: true,
+					ht:   tc,
+				},
+			}, nil
+		} else {
+			return valType{}, fmt.Errorf("%s at offset %d: invalid valtype", thing, at)
+		}
+	}
+}
+
+func (p *parser) ReadRefType(thing string) (refType, error) {
+	kind, err := p.PeekByte(thing)
+	if err != nil {
+		return refType{}, err
+	}
+
+	null := false
+	if kind == 0x64 || kind == 0x63 {
+		utils.Must1(p.ReadByte(thing))
+		null = kind == 0x63
+	}
+
+	ht, err := p.ReadHeapType(thing)
+	if err != nil {
+		return refType{}, err
+	}
+
+	return refType{
+		null: null,
+		ht:   ht,
+	}, nil
+}
+
+func (p *parser) ReadHeapType(thing string) (typeCode, error) {
+	at := p.cur
+	kind, n, err := p.ReadS64(thing)
+	if err != nil {
+		return 0, err
+	}
+	if kind < 0 && n != 1 {
+		return 0, fmt.Errorf("%s at offset %d: invalid abstract heap type", thing, at)
+	}
+	ht := typeCode(kind)
+	if !ht.IsHeapType() {
+		return 0, fmt.Errorf("%s at offset %d: invalid heap type", thing, at)
+	}
+	return ht, nil
+}
+
+func (p *parser) ReadLimits(thing string) (limits, error) {
+	flags, err := p.ReadByte("limits flags")
+	if err != nil {
+		return limits{}, err
+	}
+
+	min, _, err := p.ReadU64("limits min")
+	if err != nil {
+		return limits{}, err
+	}
+
+	lim := limits{min: min}
+	if flags&0b001 > 0 {
+		max, _, err := p.ReadU64("limits max")
+		if err != nil {
+			return limits{}, err
+		}
+		lim.hasMax = true
+		lim.max = max
+	}
+	if flags&0b100 > 0 {
+		lim.at = atI64
+	}
+
+	return lim, nil
 }
 
 func (p *parser) Expect(thing string, bytes []byte) error {
---
+++ isolate/types.go
@@ -0,0 +1,133 @@
+package isolate
+
+import "fmt"
+
+type memType struct {
+	lim limits
+}
+
+type tableType struct {
+	et  refType
+	lim limits
+}
+
+type globalType struct {
+	mut bool
+	t   valType
+}
+
+type addressType int
+
+const (
+	atI32 addressType = iota
+	atI64
+)
+
+type limits struct {
+	at       addressType
+	min, max uint64
+	hasMax   bool
+}
+
+type valType struct {
+	isRef        bool
+	numOrVecType typeCode
+	refType      refType
+}
+
+func (vt valType) IsNumType() bool {
+	return !vt.isRef && vt.numOrVecType.IsNumType()
+}
+
+func (vt valType) IsVecType() bool {
+	return !vt.isRef && vt.numOrVecType.IsVecType()
+}
+
+func (vt valType) IsRefType() bool {
+	return vt.isRef
+}
+
+func (vt valType) NumType() typeCode {
+	if !vt.IsNumType() {
+		panic(fmt.Errorf("valtype %d was not a numtype", vt))
+	}
+	return vt.numOrVecType
+}
+
+func (vt valType) VecType() typeCode {
+	if !vt.IsVecType() {
+		panic(fmt.Errorf("valtype %d was not a vectype", vt))
+	}
+	return vt.numOrVecType
+}
+
+func (vt valType) RefType() refType {
+	if !vt.IsRefType() {
+		panic(fmt.Errorf("valtype %d was not a reftype", vt))
+	}
+	return vt.refType
+}
+
+type refType struct {
+	null bool
+	ht   typeCode // may be an abstract heap type or a concrete one, depending on sign
+}
+
+type typeCode int
+
+const (
+	// The hex bytes in here refer to the number's encoding in SLEB128.
+
+	// numtype
+	nt__last  typeCode = ntI32
+	ntI32     typeCode = -1 // 0x7F
+	ntI64     typeCode = -2 // 0x7E
+	ntF32     typeCode = -3 // 0x7D
+	ntF64     typeCode = -4 // 0x7C
+	nt__first typeCode = ntF64
+
+	// vectype
+	vt__last  typeCode = vtV128
+	vtV128    typeCode = -5 // 0x7B
+	vt__first typeCode = vtV128
+
+	// heaptype (abstract, because positive values mean concrete type index)
+	ht__last   typeCode = htNoExn
+	htNoExn    typeCode = -12 // 0x74
+	htNoFunc   typeCode = -13 // 0x73
+	htNoExtern typeCode = -14 // 0x72
+	htNone     typeCode = -15 // 0x71
+	htFunc     typeCode = -16 // 0x70
+	htExtern   typeCode = -17 // 0x6F
+	htAny      typeCode = -18 // 0x6E
+	htEq       typeCode = -19 // 0x6D
+	htI31      typeCode = -20 // 0x6C
+	htStruct   typeCode = -21 // 0x6B
+	htArray    typeCode = -22 // 0x6A
+	htExn      typeCode = -23 // 0x69
+	ht__first  typeCode = htExn
+
+	// Sentinel bytes indicating that a ref type's heap type follows.
+	__rtNonNull typeCode = -28 // 0x64
+	__rtNull    typeCode = -29 // 0x63
+)
+
+func (tc typeCode) IsNumType() bool {
+	return nt__first <= tc && tc <= nt__last
+}
+
+func (tc typeCode) IsVecType() bool {
+	return vt__first <= tc && tc <= vt__last
+}
+
+func (tc typeCode) IsHeapType() bool {
+	return tc.IsAbstractHeapType() || tc.IsConcreteHeapType()
+}
+
+func (tc typeCode) IsAbstractHeapType() bool {
+	return ht__first <= tc && tc <= ht__last
+}
+
+func (tc typeCode) IsConcreteHeapType() bool {
+	return tc > 0
+}
