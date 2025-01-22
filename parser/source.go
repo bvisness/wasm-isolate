dcba34a0951c3b3dd0aa8649a2d684b1db946d00
Ben Visness <bvisness@mozilla.com>
2025-01-22 16:56:36 -0600

    WIP haha

--- parser/source.go
+++ parser/source.go
@@ -1,5 +1,7 @@
 package parser
 
+import "math"
+
 // Manual translation of source.ml
 
 type Pos struct {
@@ -16,4 +18,29 @@ type Region struct {
 type Phrase[T any] struct {
 	at *Region
 	it T
+}
+
+func _operatorAtAt_2[T any](x T, region *Region) *Phrase[T] {
+	return &Phrase[T]{
+		it: x,
+		at: region,
+	}
+}
+
+var _no_pos = &Pos{
+	file:   "",
+	line:   0,
+	column: 0,
+}
+
+var _no_region = &Region{
+	left:  _no_pos,
+	right: _no_pos,
+}
+
+func _all_region(file string) *Region {
+	return &Region{
+		left:  &Pos{file, 0, 0},
+		right: &Pos{file, math.MaxInt, math.MaxInt},
+	}
 }
